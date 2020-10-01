0b83588fac0d04ef817b9576f5a603fb2c1b0db9
Hub-Abhi <62337400+Hub-Abhi@users.noreply.github.com>
2020-10-01 23:06:03 +0530

    Update Sort1.c

--- Sort1.c
+++ Sort1.c
@@ -24,18 +24,18 @@ int binarySearch(int *arr, int l, int r)
      	{ 
        		 int mid= (l + r)/2;
        		 if (*(arr+mid)==1 && *(arr+mid-1)==0)  
-           		 return mid; 
+           		   return mid; 
 		 else if(*(arr+mid)==0 && *(arr+mid+1)==1)
            		   return (mid+1);
        		 else if (*(arr+mid)==1 && *(arr+mid-1)==1) 
-        			    return binarySearch(arr, l, mid-1); 
+        		   return binarySearch(arr, l, mid-1); 
     	}
 } 
 void printArray(int *arr, int n) 
 { 
     for(int i=0;i<n;i++)
      printf("%d ",*(arr+i));
-    printf("\n");
+     printf("\n");
 } 
 int main() 
 { 
@@ -45,9 +45,9 @@ int main()
     arr=(int*)malloc(n*sizeof(int));
     printf("\nEnter the binary array elements: ");
     for(i=0;i<n;i++)
-     scanf("%d",arr+i);
+    scanf("%d",arr+i);
     insertionSort(arr, n);
-     printf("\nThe array elements are:\n");
+    printf("\nThe array elements are:\n");
     printArray(arr, n);
     int result = binarySearch(arr, 0, n - 1);
     printf("\nElement 1 is first present at index %d and at position %d.\n",result,result+1);
